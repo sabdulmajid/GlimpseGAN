833b09b60324e6e23518a974358400f9b7978253
sabdulmajid <ayman.hasib@outlook.com>
2023-07-14 19:34:31 +0300

    initializing the file

--- train_GAN.py
+++ train_GAN.py
@@ -4,3 +4,14 @@ import torch.optim as optim
 from torchvision.datasets import MNIST
 from torchvision import transforms
 from torch.utils.data import DataLoader
+
+
+torch.manual_seed(42)
+
+latent_dim = 100
+img_shape = 28 * 28
+epochs = 100
+batch_size = 128
+
+generator = Generator(latent_dim, img_shape)
+discriminator = Discriminator(img_shape)
